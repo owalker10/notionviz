aca467b879503af5095547d5e1210cae642edead
owalker10 <36974651+owalker10@users.noreply.github.com>
2021-06-06 21:36:01 -0500

    service account base64

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,8 +5,10 @@ import admin = require("firebase-admin");
 
 require("dotenv").config();
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT || "";
-console.log("service account", serviceAccount !== "");
+const serviceAccount: string = Buffer.from(
+  process.env.FIREBASE_SERVICE_ACCOUNT || "",
+  "base64"
+).toString("ascii");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
